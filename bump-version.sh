73fe30dccb7ae130fd6c763a391dfa35e554abe4
nimaaskarian <nimaaskarian.work@outlook.com>
2025-03-06 11:00:46 +0330

    Add build.sh

--- bump-version.sh
+++ bump-version.sh
@@ -8,15 +8,8 @@ bump_version() {
   git add Cargo.toml
 }
 
-build_package() {
-  cargo build --release || echo_exit Linux build failed.
-  cargo build --release --target x86_64-pc-windows-gnu || echo_exit Windows build failed.
-  export ANDROID_NDK_HOME=/opt/android-sdk/ndk/27.0.12077973
-  cargo ndk -t aarch64-linux-android build --release || echo_exit Termux build failed.
-}
-
 cargo test || echo_exit Unittests failed.
 bump_version
-build_package
+. $(dirname $0)/build.sh
 git add Cargo.lock
 git commit -m "Bumped version $TAG"
